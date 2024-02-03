7af50dc93d94286fa542a8957a785b981784e7ec
Shauna L <slachelier@gmail.com>
2024-02-03 17:01:33 -0600

    fixed intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -41,7 +41,7 @@ export default function Intro() {
 
 
       <motion.h1 className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl' initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.4 }}>
-        <span className='font-bold'>Hello, I'm Shauna.</span> I am a <span className='font-bold'>full-st ack developer</span> with <span className='font-bold'>three years</span> of experience developing projects. <br/>I specialize in <span className='font-bold'>React (Next.js).</span>
+        <span className='font-bold'>Hello, I'm Shauna.</span> I am a <span className='font-bold'>full-stack developer</span> with <span className='font-bold'>two years</span> of experience developing projects. <br/>I specialize in <span className='font-bold'>React (Next.js).</span>
       </motion.h1>
 
     <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
