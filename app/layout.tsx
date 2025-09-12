c65c60a740c7f3a78b48b6e8eb95059a5aea796d
Shauna Lachelier <70386413+SLachelier@users.noreply.github.com>
2025-09-12 16:07:46 -0500

    Update experience in portfolio description

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Shauna Lachelier | Portfolio',
-  description: 'Shauna Lachelier is a full-stack developer with 3 years of experience.',
+  description: 'Shauna Lachelier is a full-stack developer with 4 years of experience.',
 }
 
 export default function RootLayout({
